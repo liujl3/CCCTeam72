7acb3196e15bbe8920b30dfed4c22542352cfb2a
TheTernip <65105875+TheTernip@users.noreply.github.com>
2020-05-23 02:02:07 +0800

    fix bug

--- DataAnalysis/Analysis.py
+++ DataAnalysis/Analysis.py
@@ -25,23 +25,22 @@ def gather(db):
     lang = []
     geo = Nominatim()
     for item in db.view("_design/newDesign/_view/new-view"):
-        tweet_id = db[id]
+        tweet_id = item.value['id']
         if tweet_id not in ids:
             ids.append(tweet_id)
             longitude = 0
             latitude = 0
             for i in range(0,3):
-                longitude += item.place['bounding_box']['coordinates'][0][i][0]
-                latitude += item.place['bounding_box']['coordinates'][0][i][1]
+                longitude += item.value['place']['bounding_box']['coordinates'][0][i][0]
+                latitude += item.value['place']['bounding_box']['coordinates'][0][i][1]
             coordinates.append([longitude/4,latitude/4])
-            full_name.append(item.place['full_name'])
-            lang.append(item['lang'])
+            full_name.append(item.value['place']['full_name'])
+            lang.append(item.value['lang'])
 
     data = {'full_name': full_name, 'coordinates': coordinates}
     df = pd.DataFrame(data=data)
-
-    city = full_name.str.split(',', expand=True)
-    data_lang = {'city': city.icol(0), 'state': city.icol(1), 'coordinates': coordinates, 'lang': lang,'count':[]}
+    city = pd.DataFrame({'content':full_name})['content'].str.split(',', expand=True)
+    data_lang = {'city': city[0].values.tolist(), 'state': city[1].values.tolist(), 'coordinates': coordinates, 'lang': lang,'count':ids}
     dlf = pd.DataFrame(data=data_lang)
     city_lang = dlf.groupby(['city','lang']).count()
     clf = pd.DataFrame(city_lang)["count"].reset_index(name="Count")
@@ -55,7 +54,7 @@ def tweets_count(df):
     city_df = pd.DataFrame(city_NumTweet)["coordinates"].reset_index(name="Tweets_Num")
     full_name = city_df['full_name']
     city = full_name.str.split(',', expand=True)
-    city_state = pd.DataFrame({'full_name': full_name, 'city': city.icol(0), 'state': city.icol(1)})
+    city_state = pd.DataFrame({'full_name': full_name, 'city': city[0].values.tolist(), 'state': city[1].values.tolist()})
     city_df = pd.merge(city_df, city_state, on='full_name')
     return city_df
 # full_name city state Tweets_Num Hospital_Num
@@ -64,13 +63,16 @@ def data_combine(tweet_df, aurin_df):
     geolocator = Nominatim()
     latitude = aurin_df[' latitude'].values.tolist()
     longitude = aurin_df[' longitude'].values.tolist()
-    state = state_code[aurin_df[' state']]
+    state = aurin_df[' state'].values.tolist()
+    for i in range(len(state)):
+        state[i] = state_code[state[i]]
+
     coordinates = []
     city_list = tweet_df['city'].values.tolist()
     city_aurin = []
     for i in range(len(latitude)):
         hasCity = False
-        city_name = geolocator.reverse(str(latitude) + "," + str(longitude))
+        city_name = geolocator.reverse(str(latitude[i]) + "," + str(longitude[i]))
         coordinates.append([longitude[i], latitude[i]])
         for j in range(len(city_list)):
             if city_list[j] in city_name.address:
@@ -88,7 +90,7 @@ def data_combine(tweet_df, aurin_df):
     citys_hospital = pd.DataFrame({'full_name':full_name,'city': city_aurin, 'state': state, 'coordinate': coordinates})
     hospital_num = citys_hospital.groupby['city']
     new_aurin_df = pd.DataFrame(hospital_num)['coordinate'].reset_index(name="Hospital_Num")
-    final_df = pd.merge(tweet_df, new_aurin_df, on=['full_name', 'city', 'state'])
+    final_df = pd.merge(tweet_df, new_aurin_df, on='city')
 
     return final_df
 
@@ -103,7 +105,7 @@ def save_result(final_df, database_name):
         couch = couch.create(database_name)
 
     final_json = final_df.to_json(orient="columns", force_ascii=False)
-    couch.save(final_json)
+    couch.save(json.loads(final_json))
 
 
 def main():
@@ -116,3 +118,5 @@ def main():
     aurin_df = pd.DataFrame(aurin_file)
     final_df = data_combine(count_df, aurin_df)
     save_result(final_df, result_database)
+
+main()
